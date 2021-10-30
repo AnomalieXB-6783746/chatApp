79482dd6059c5e2cd1c7f85e855a57a8090a683c
AnomalieXB-6783746 <56471811+AnomalieXB-6783746@users.noreply.github.com>
2021-10-30 18:21:08 +0200

    use proper templating

--- chat/main.go
+++ chat/main.go
@@ -3,22 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
 
 func main() {
 	// listen on the root path
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		//respond HTML
-		writer.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Let's chat!
-				</body>
-			</html>`))
-	})
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
